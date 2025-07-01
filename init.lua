b9160a4285dd5e0a6e1a6801e0985b94e3d3d84f
Aron Szakacs <aron.szakacs@onsemi.com>
2025-07-01 13:55:48 +0200

    add text-case plugin

--- init.lua
+++ init.lua
@@ -1007,6 +1007,31 @@ require('lazy').setup({
   -- Or use telescope!
   -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
   -- you can continue same window with `<space>sr` which resumes last telescope search
+
+  {
+  "johmsalas/text-case.nvim",
+  dependencies = { "nvim-telescope/telescope.nvim" },
+  config = function()
+    require("textcase").setup({})
+    require("telescope").load_extension("textcase")
+  end,
+  keys = {
+    "ga", -- Default invocation prefix
+    { "ga.", "<cmd>TextCaseOpenTelescope<CR>", mode = { "n", "x" }, desc = "Telescope" },
+  },
+  cmd = {
+    -- NOTE: The Subs command name can be customized via the option "substitude_command_name"
+    "Subs",
+    "TextCaseOpenTelescope",
+    "TextCaseOpenTelescopeQuickChange",
+    "TextCaseOpenTelescopeLSPChange",
+    "TextCaseStartReplacingCommand",
+  },
+  -- If you want to use the interactive feature of the `Subs` command right away, text-case.nvim
+  -- has to be loaded on startup. Otherwise, the interactive feature of the `Subs` will only be
+  -- available after the first executing of it or after a keymap of text-case.nvim has been used.
+  lazy = false,
+}
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
